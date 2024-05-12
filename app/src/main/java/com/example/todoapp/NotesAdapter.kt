ce7494d181e7d9e1803cd87f3612d4d0b3465d25
IT22052292 <gavesha70@gmail.com>
2024-05-12 10:39:00 +0530

    after the read starts working

--- app/src/main/java/com/example/todoapp/NotesAdapter.kt
+++ app/src/main/java/com/example/todoapp/NotesAdapter.kt
@@ -8,27 +8,33 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-class NotesAdapter(private var notes:List<Note>,context: Context): RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+class NotesAdapter(private var notes: List<Note>, context: Context) :
+    RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
-    class NoteViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-        val titleTextView:TextView =itemView.findViewById(R.id.titleTextView)
-        val contentTextView:TextView=itemView.findViewById(R.id.contentTextView)
+    class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.note_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
         return NoteViewHolder(view)
     }
 
-    override fun getItemCount(): Int =notes.size
+    override fun getItemCount(): Int {
+        return notes.size
+    }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val note = notes[position]
-        holder.titleTextView.text=note.title
-        holder.contentTextView.text=note.content
+        holder.titleTextView.text = note.title
+        holder.contentTextView.text = note.content
     }
-    fun refreshData(newNotes:List<Note>){
-        notes=newNotes
+
+    fun refreshData(newNotes: List<Note>) {
+        notes = newNotes
         notifyDataSetChanged()
     }
+
+
 }
